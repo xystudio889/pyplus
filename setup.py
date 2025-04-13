c8060e3ee1bda9ccf8c7640a2a5e82e775e1e711
xystudio889 <173288240@qq.com>
2025-04-13 21:46:05 +0800

    1.2.1.post0

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ from pyplus import get_pre_version
 data_files = [('Scripts', [])]
 
 if sys.platform == "win32":
-    data_files = [('Scripts', ['scripts/pyplus.exe'])]
+    data_files = [('Scripts', [])]#'scripts/pyplus.exe'])]
 
 setup(
     name="python-plus-tools",
-    version="1.2.2b1",
+    version="1.2.1.post0",
     packages=find_packages(),
     install_requires=["toml>=0.10", "indently-decorators>=1.0,<=1.1", "cryptography>=3.4", "imgfit>=0.3", "colorama>=0.1", 
                       "numpy>=1.14", "matplotlib>=3.4","torch>=2", "pandas>=2"],
