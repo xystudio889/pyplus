285a9b771a9f3b4a9cce4c83fe352bda5eb8cc6e
xystudio889 <173288240@qq.com>
2025-04-15 22:22:33 +0800

    1.2.0.post2

--- pyplus/tools/__init__.py
+++ pyplus/tools/__init__.py
@@ -1,2 +1,3 @@
 from . import dataList_conversion, jurisdiction, ptime, pydebugger, tag, update, dec, type, moudle, variables, colors#, password
+from . import jurisdiction,operators, dataList_conversion # deprecated moudle
 import decorators
