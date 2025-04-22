21454e81e622fe714b2f2004b9e34624d6cab2fa
xystudio889 <173288240@qq.com>
2025-04-22 20:40:10 +0800

    create requirements.txt

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="python-plus-tools",
-    version="1.3.0a1",
+    version="1.3.0a2.dev0",
     packages=find_packages(),
     install_requires=[
         "toml>=0.10", "indently-decorators>=1.0,<=1.1", "cryptography>=3.4", "imgfit>=0.3", "colorama>=0.1", "deprecated>=1", 
