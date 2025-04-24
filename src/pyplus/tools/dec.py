b2fcc2f43d72eb684f9d2a1b39b20e566345af8c
xystudio889 <173288240@qq.com>
2025-04-24 20:14:25 +0800

    trmp a1

--- src/pyplus/tools/dec.py
+++ src/pyplus/tools/dec.py
@@ -1,43 +1,7 @@
 from .type import _AllSimpleTypes as SimpleType
 from .stack import IteratorCalculator
+from advancedlib.operator.matical import select, space, cmp
 from typing_extensions import override, Union, TypeAlias
-
-def select(a:object, b:object ,select: bool) -> object:
-    '''
-    Select a value.
-
-    :example: 
-    >>> import pyplus
-    >>> pyplus.tools.dec.select(1, 2, False)
-    >>> 1
-    >>> pyplus.tools.dec.select(1, 2, True)
-    >>> 2
-    
-    :param bool select: If select == True,return b, else return a.
-    :param object a: If select == False,return a.
-    :param object b: If select == True,return b.
-
-    :return object: If select == True,return b, else return a.
-    '''
-    return b if select else a
-
-def space(a, b) -> int:
-    '''
-    :example: 
-    >>> import pyplus
-    >>> pyplus.tools.dec.space(1, 2)
-    >>> -1
-    >>> pyplus.tools.dec.select(2, 2)
-    >>> 0
-    >>> pyplus.tools.dec.select(3, 2)
-    >>> 1
-
-    :return int: 
-    If a < b,return -1.If a > b,return 1.If a == b,return 0.
-    '''
-    return (a > b) - (a < b)
-
-cmp = space
 
 class dint(int):
     def __new__(cls, value:int):
