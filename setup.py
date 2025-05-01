801608a0434acdca24b349c064d2530a4bc40e1c
xystudio <173288240@qq.com>
2025-05-01 19:55:53 +0800

    update pyproject.toml

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="python-plus-tools",
-    version="2.1.0.dev0",
+    version="2.0.1",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     install_requires=[
