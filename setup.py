78d1c4a1ab52f3fff81b6cd79cec40cb728ea120
xystudio <173288240@qq.com>
2025-05-01 19:48:46 +0800

    2.0.1

--- setup.py
+++ setup.py
@@ -53,5 +53,6 @@ setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ]
+    ], 
+    keywords=["debug", "science", "pyplus"]
 )
