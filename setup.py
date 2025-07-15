125ad9a74d29ce4092fa174218307e4b5b462f26
xystudio889 <173288240@qq.com>
2025-07-15 19:53:16 +0800

    3.0.0.dev0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="python-plus-tools",
-    version="2.2.0a2",
+    version="3.0.0.dev0",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     install_requires=[
